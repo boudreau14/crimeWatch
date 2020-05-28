a958c38b3e8e742127fd43a94b08e6359fa44293
boudreau14 <60114894+boudreau14@users.noreply.github.com>
2020-05-27 23:03:02 -0400

    Commented script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,56 +1,40 @@
 $(document).ready(function () {
-    // DO NOT TOUCH THIS LINE BELOW
-    // $('.mdb-select').materialSelect();
+  //Submit button on event for report a crime page
+  $(".report-form").on("submit", function (event) {
+    event.preventDefault();
 
+    var hood_id = $(this).children(".hood_id").val();
+    var date_id = $(this).children(".date_id").val();
+    var arrest_id = $(this).children(".arrest_id").val();
+    var type_id = $(this).children(".type_id").val();
+    var desc_id = $(this).children(".desc_id").val();
 
-    $(".report-form").on("submit", function (event) {
-        event.preventDefault();
-  
-        var hood_id = $(this).children(".hood_id").val();
-        var date_id = $(this).children(".date_id").val();
-        var arrest_id = $(this).children(".arrest_id").val();
-        var type_id = $(this).children(".type_id").val();
-        var desc_id = $(this).children(".desc_id").val();
-
-        console.log(hood_id, date_id, arrest_id, type_id, desc_id)
-        $.ajax({
-            method: "POST",
-            url: "/report/" + hood_id, date_id, arrest_id, type_id, desc_id
-        }).then(function (data) {
-            // SHOULD WE MAKE A NEW TABLE JUST FOR WHEN THINGS ARE REPORTED??
-            location.reload();
-        })
-  
-    })
-// ("submit", function (event)
-    $(".searchForm").on("submit", function(event) { 
-        event.preventDefault();
-        debugger
-        // var loc = [];
-        //var neighborhood = $('#hood').find(':selected').val()
-        var neighborhood = "{\"lat\":\"41.987\",\"long\":\"-87.662\"}"
-        // loc.push(neighborhood)
-        console.log(neighborhood);
-        // neighborhood = event.target.value;
-        // loc.push(neighborhood);
-        // console.log(neighborhood);
-        // console.log(loc)
-        
-    
-
-  
-        // // var neighborhood = $(this).children(".neighborhood").val();
-
-        // console.log(event)
-        $.ajax({
-            method: "GET",
-            url: "/map/neighborhood?neighborhood=" + neighborhood
-        }).then(function (data) {
-            // SHOULD WE MAKE A NEW TABLE JUST FOR WHEN THINGS ARE REPORTED??
-            // location.reload();
-            console.log(data)
-        })
-
+    console.log(hood_id, date_id, arrest_id, type_id, desc_id);
+    $.ajax({
+      method: "POST",
+      url: "/report/" + hood_id,
+      date_id,
+      arrest_id,
+      type_id,
+      desc_id,
+    }).then(function (data) {
+      location.reload();
+    });
   });
 
+  // ++++++ Future development for SEARCH function
+  //   $(".searchForm").on("submit", function (event) {
+  //     event.preventDefault();
+
+  //     var neighborhood = '{"lat":"41.987","long":"-87.662"}';
+
+  //     console.log(neighborhood);
+
+  //     $.ajax({
+  //       method: "GET",
+  //       url: "/map/neighborhood?neighborhood=" + neighborhood,
+  //     }).then(function (data) {
+  //       console.log(data);
+  //     });
+  //   });
 });

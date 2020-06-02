75b99921404ef3cdaba8606426a8e852d99029e1
boudreau14 <60114894+boudreau14@users.noreply.github.com>
2020-06-02 13:19:35 -0400

    Final version

--- db/crimes_db_crimes_reported.sql
+++ db/crimes_db_crimes_reported.sql
@@ -4,7 +4,7 @@ create table crimes_reported (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     neighborhood VARCHAR(100) NOT NULL,
     date TEXT,
-    police_called boolean,
+    police_called VARCHAR(10),
     type VARCHAR(100) NOT NULL,
     notes VARCHAR(300)    
 )
